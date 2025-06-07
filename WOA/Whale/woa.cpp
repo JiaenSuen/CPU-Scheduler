0747e984ce6a256fb9e087ce768ec2ef006bcc74
JiaenSuen <john20050826@gmail.com>
2025-06-07 21:19:23 +0800

    Create woa.cpp

---
+++ WOA/Whale/woa.cpp
@@ -0,0 +1,20 @@
+#include "include/modules.hpp"
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <numeric>
+#include <random>
+
+using namespace std;
+using namespace os_display;
+using namespace std::chrono;
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
